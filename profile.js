7a11012b187523831cd840f5a7f9abf5c0d6075f
Jiewei Qian <qjw@wacky.one>
2018-10-25 15:09:43 +1100

    refactor & sign-off profile, read-gz, (breaking)

--- profile.js
+++ profile.js
@@ -1,45 +1,52 @@
 const fs = require('fs').promises
-const { join, extname } = require('path')
-const async = require('async')
+const { join } = require('path')
+const { isGzJson, readGzJson } = require('./lib/read-gz')
+const asyncMap = require('./lib/async-map')
 
-function asyncMap(coll, limit, fn) {
-    return new Promise((resolve, reject) => {
-        async.mapLimit(coll, limit, fn, (err, results) => !err ? resolve(results) : reject(err))
-    })
-}
-
+// round to 2 decimal point, useful for percentage
 function round2(n) {
     return Math.round(n * 100) / 100
 }
 
+//
+// read commit JSON, print extract fields as CSV
+//
+// print to stdout: extracted fields in CSV format
+// print to stderr: statistics about duplicated repositories
+//
 module.exports = async function ghdcDataProfile(opts) {
     const dataDir = opts.dataDir
     const commitDir = join(dataDir, 'commits')
 
     const commitFiles = await fs.readdir(commitDir)
-    const sha = new Set()
-    const lang = {}
-    const langDedupe = {}
 
-    // NOTE: do not read all files into memory. Memory may be insufficient
-    //       do stream processing
+    // statistic related variables
+    let sha = new Set()
+    let lang = {}
+    let langDedupe = {}
+
+    // NOTE: do not read all files into memory. Memory may be insufficient,
+    //       do stream processing instead.
+
+    // write CSV header
     console.log('repo,stars,forks,issues,lang,n_files,addition,deletion,sha')
     await asyncMap(
-        commitFiles.filter(filename => extname(filename) === '.json'),
+        commitFiles.filter(isGzJson),
         64,
         async filename => {
-            const {
-                commit,
-                repo
-            } = await fs.readFile(join(commitDir, filename), { encoding: 'utf-8' }).then(str => JSON.parse(str))
+            const { commit, repo } = await readGzJson(join(commitDir, filename))
 
+            // record language
             lang[repo.language] = (lang[repo.language] || 0) + 1
 
+            // deduplicate, skip if commit is already seen
             if (sha.has(commit.sha)) return
             sha.add(commit.sha)
 
+            // record language for unique commits
             langDedupe[repo.language] = (langDedupe[repo.language] || 0) + 1
 
+            // build CSV record, should match with header
             const line = [
                 repo.full_name,
                 repo.stargazers_count,
@@ -55,6 +62,9 @@ module.exports = async function ghdcDataProfile(opts) {
         }
     )
 
+    // print to stderr:
+    // statistics about per language duplication
+    // only care for languages with (>100) of duplicates
     console.error(`distinct commits: ${ sha.size } / ${ commitFiles.length }, ${ round2(sha.size / commitFiles.length * 100) }%`)
     console.error('per language: <deduped> / <raw>')
     for (const key in lang) {
@@ -62,5 +72,4 @@ module.exports = async function ghdcDataProfile(opts) {
             console.error(`\t${key}: ${langDedupe[key]} / ${lang[key]}, ${ round2(langDedupe[key] / lang[key] * 100) }%`)
         }
     }
-
 }
